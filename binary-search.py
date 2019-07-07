2d4711600801cce33ef773c07ae8b58d35a9c646
Rami <51302402+ramisal404@users.noreply.github.com>
2019-07-07 14:47:13 +0200

    Create binary-search.py

---
+++ binary-search.py
@@ -0,0 +1,19 @@
+
+def binary_search(sorted_list, target):
+  if not sorted_list:
+    return 'value not found'
+  mid_idx = len(sorted_list)//2
+  mid_val = sorted_list[mid_idx]
+  if mid_val == target:
+    return mid_idx
+  if mid_val > target:
+    left_half = sorted_list[:mid_idx]
+    return binary_search(left_half, target)
+  if mid_val < target:
+    right_half = sorted_list[mid_idx+1:]
+    result = binary_search(right_half, target)
+    if result == "value not found":
+      return result
+    else:
+      return result + mid_idx + 1
+      
